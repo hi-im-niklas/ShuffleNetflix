282d4b48bb04b24b2217eeaf82e4292e7383872b
Niklas <nickbonn007@googlemail.com>
2021-06-27 02:48:19 +0200

    Model - Episoden/Serien Interface erstellt

---
+++ ShuffleWatchList/ShuffleWatchList/IEpisode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShuffleWatchList
+{
+    interface IEpisode 
+    {
+        public string Name { get; set; }
+
+        public int Season { get; set; }
+
+        public int Episode { get; set; }
+
+        public ISerie Serie { get; set; }
+
+        public string Url { get; set; }
+    }
+}
