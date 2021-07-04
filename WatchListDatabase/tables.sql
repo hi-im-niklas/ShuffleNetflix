cfcd32858707e198f81d0f57db1480ee8e2e1763
hi-im-niklas <86476414+hi-im-niklas@users.noreply.github.com>
2021-07-04 16:03:26 +0200

    Add files via upload

--- WatchListDatabase/tables.sql
+++ WatchListDatabase/tables.sql
@@ -3,6 +3,7 @@ CREATE TABLE tUser
 	UserID integer NOT NULL AUTO_INCREMENT,
     UserName varchar(40),
     Passwort varchar(40),
+    Email varchar(40),
     Vorname varchar(40),
     Nachname varchar(80),
     PRIMARY KEY (UserID)
