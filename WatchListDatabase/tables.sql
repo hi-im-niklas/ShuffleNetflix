9fafb7b47e537c6376adc47f30f78397fc590bf3
hi-im-niklas <86476414+hi-im-niklas@users.noreply.github.com>
2021-07-04 15:09:52 +0200

    Add files via upload

--- WatchListDatabase/tables.sql
+++ WatchListDatabase/tables.sql
@@ -9,5 +9,23 @@ CREATE TABLE tUser
 );
 
 CREATE TABLE tSerie
+(
+	SerienID integer NOT NULL AUTO_INCREMENT,
+    SerienName varchar(40),
+    Beschreibung varchar(255),
+    PRIMARY KEY (SerienID)
+);
+
+CREATE TABLE tEpisode
+(
+	EpisodeID integer NOT NULL AUTO_INCREMENT,
+    EpiName varchar(40),
+    Season integer,
+    EpisodeNum integer,
+    URL varchar(255),
+    SerienID integer,
+    PRIMARY KEY (EpisodeID),
+    FOREIGN KEY (SerienID) REFERENCES tSerie(SerienID)
+);
     
     
